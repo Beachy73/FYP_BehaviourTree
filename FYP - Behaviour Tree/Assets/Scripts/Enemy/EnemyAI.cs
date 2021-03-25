324560c44abdcdd29351b920ab116b0dde47ec83
Zach <up898620@myport.ac.uk>
2021-03-25 10:13:01 +0000

    Added some movement behaviours

--- FYP - Behaviour Tree/Assets/Scripts/Enemy/EnemyAI.cs
+++ FYP - Behaviour Tree/Assets/Scripts/Enemy/EnemyAI.cs
@@ -11,6 +11,15 @@ public class EnemyAI : MonoBehaviour
     private EnemyBB bb;
     private BTNode BTRootNode;
 
+    [SerializeField]
+    private float moveSpeed = 5f;
+
+    public Vector3 dir;
+    private Vector3 moveLocation;
+    public bool isMoving = false;
+    private Quaternion lookRotation;
+    [SerializeField] private float rotationDamping = 8f;
+
     #endregion
 
 
@@ -28,9 +37,7 @@ public class EnemyAI : MonoBehaviour
         Selector rootChild = new Selector(bb);
         BTRootNode = rootChild;
 
-
-
-
+        
 
 
 
@@ -41,11 +48,88 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isMoving)
+        {
+            dir = moveLocation - transform.position;
+            transform.position += dir.normalized * moveSpeed * Time.deltaTime;
+
+            lookRotation = Quaternion.LookRotation(dir);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationDamping);
+        }
     }
 
     public void ExecuteBT()
     {
         BTRootNode.Execute();
+    }
+
+    public void MoveTo(Vector3 moveLocation)
+    {
+        isMoving = true;
+        this.moveLocation = moveLocation;
+    }
+
+    public void StopMovement()
+    {
+        isMoving = false;
+    }
+}
+
+
+public class MoveTo : BTNode
+{
+    private EnemyBB eBB;
+    private EnemyAI enemyRef;
+
+    public MoveTo(EnemyBB bb, EnemyAI enemy) : base(bb)
+    {
+        eBB = (EnemyBB)bb;
+        enemyRef = enemy;
+    }
+
+    public override BTStatus Execute()
+    {
+        enemyRef.MoveTo(eBB.moveToLocation);
+        return BTStatus.SUCCESS;
+    }
+}
+
+public class WaitTillAtLocation : BTNode
+{
+    private EnemyBB eBB;
+    private EnemyAI enemyRef;
+
+    public WaitTillAtLocation(Blackboard bb, EnemyAI enemy) : base(bb)
+    {
+        eBB = (EnemyBB)bb;
+        enemyRef = enemy;
+    }
+
+    public override BTStatus Execute()
+    {
+        BTStatus currentStatus = BTStatus.RUNNING;
+
+        if (Vector3.Distance(enemyRef.transform.position, eBB.moveToLocation) <= 1.0f)
+        {
+            currentStatus = BTStatus.SUCCESS;
+        }
+
+        return currentStatus;
+    }
+}
+
+public class StopMovement : BTNode
+{
+    private EnemyAI enemyRef;
+
+    public StopMovement(Blackboard bb, EnemyAI enemy) : base(bb)
+    {
+        enemyRef = enemy;
+    }
+
+    public override BTStatus Execute()
+    {
+        enemyRef.StopMovement();
+        return BTStatus.SUCCESS;
     }
 }
--- FYP - Behaviour Tree/Assets/Scripts/Enemy/EnemyBB.cs
+++ FYP - Behaviour Tree/Assets/Scripts/Enemy/EnemyBB.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class EnemyBB : Blackboard
 {
+    public Vector3 moveToLocation;
+    
     // Start is called before the first frame update
     void Start()
     {
