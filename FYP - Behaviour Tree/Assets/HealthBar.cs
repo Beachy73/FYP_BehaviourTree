75e901909e899b8a095185f661b916d39ae525e0
Zach <up898620@myport.ac.uk>
2021-05-22 15:48:37 +0100

    Health bar colour change added

--- FYP - Behaviour Tree/Assets/HealthBar.cs
+++ FYP - Behaviour Tree/Assets/HealthBar.cs
@@ -6,15 +6,21 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
+    public Gradient gradient;
+    public Image fill;
 
     public void SetMaxHealth(float maxHealth)
     {
         slider.maxValue = maxHealth;
         slider.value = maxHealth;
+
+        fill.color = gradient.Evaluate(1f);
     }
     
     public void SetHealth(float health)
     {
         slider.value = health;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
